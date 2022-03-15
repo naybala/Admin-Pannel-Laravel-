adc07d34c0216c30fc944ec6af8226ffbc3f960f
naybala <naybalaismylife.nbl@gmail.com>
2022-03-15 13:16:29 +0630

    adding middle ware user and admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\AdminCheckMiddleware;
+use App\Http\Middleware\UserCheckMiddleware;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -34,7 +36,7 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
     //Admin Route Group
-    Route::get('profile', 'AdminController@profile')->name('admin#profile');
+    Route::get('profile', 'AdminController@profile')->name('admin#profile')->middleware(AdminCheckMiddleware::class);
     Route::get('logoutCancel', 'AdminController@logoutCancel')->name('admin#logoutCancel');
     Route::get('logoutConfirm', 'AdminController@logoutConfirm')->name('admin#logoutConfirm');
     Route::post('updateInfo/{id}', 'AdminController@updateInfo')->name('admin#updateInfo');
@@ -75,7 +77,7 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
 });
 Route::group(['prefix' => 'user', 'namespace' => 'User'], function () {
     //User Route Group
-    Route::get('/', 'UserController@index')->name('user#index');
+    Route::get('/', 'UserController@index')->name('user#index')->middleware(UserCheckMiddleware::class);
     Route::get('logoutCancel', 'UserController@logoutCancel')->name('user#logoutCancel');
     Route::get('logoutConfirm','UserController@logoutConfirm')->name('user#logoutConfirm');
     Route::get('categoryPizzaList/{id}', 'UserController@categoryPizzaList')->name('user#categoryPizzaList');

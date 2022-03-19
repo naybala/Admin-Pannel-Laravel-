48107aa0f29d5e989e31f7ad37740addacb5f1b8
naybala <naybalaismylife.nbl@gmail.com>
2022-03-19 13:48:34 +0630

    api test

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Response;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('categrory/list',function(){
+    $category = Category::get();
+    return Response::json($category);
 });
